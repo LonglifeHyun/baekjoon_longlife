13aece1e0a13e3689f45d5c5fe07ce450cda984a
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2021-01-20 23:23:22 +0900

    210120_1d_array_day1
    코드 수정

--- 단계별로 풀어보기/1차원_배열/2562_최댓값.cpp
+++ 단계별로 풀어보기/1차원_배열/2562_최댓값.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(void)
 {
+	vector< pair<int,int> > nums(9);
 	
+	pair<int,int> tmp;
+	for(int i=0;i<nums.size();++i){
+		cin >> tmp.first;
+		tmp.second = i+1;
+		nums[i] = tmp;
+	}
 	
+	sort(nums.begin(), nums.end());
 	
+	cout << nums[nums.size()-1].first << endl 
+		 << nums[nums.size()-1].second << endl;
 	
 	return 0;
 }
