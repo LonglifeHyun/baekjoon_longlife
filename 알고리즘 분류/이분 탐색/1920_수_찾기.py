eac2f24b2934e16fb0e7ee0fa06ba9882ba2c16e
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2021-03-08 23:56:42 +0900

    Update 1920_수_찾기.py
    해시가 더 빠르긴 함

--- 알고리즘 분류/이분 탐색/1920_수_찾기.py
+++ 알고리즘 분류/이분 탐색/1920_수_찾기.py
@@ -1,6 +1,7 @@
 import sys, bisect
 input = sys.stdin.readline
-
+"""
+# 이분 탐색
 n = int(input())
 ns = [int(i) for i in input().split()]
 m = int(input())
@@ -11,6 +12,18 @@ ns.sort()
 for jungsoo in ms:
     idx = bisect.bisect_left(ns,jungsoo)
     if idx < len(ns) and ns[idx] == jungsoo:
+        print(1)
+    else:
+        print(0)
+"""
+# 해싱
+n = int(input())
+ns = set(int(i) for i in input().split())
+m = int(input())
+ms = [int(i) for i in input().split()]
+
+for jungsoo in ms:
+    if jungsoo in ns:
         print(1)
     else:
         print(0)
