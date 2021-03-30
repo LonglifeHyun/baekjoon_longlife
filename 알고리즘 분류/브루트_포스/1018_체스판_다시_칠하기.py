d789e43a57ab718988b1f7ee52762578a8902745
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2021-03-30 21:58:28 +0900

    210330
    체스같은건 인덱스 합으로 풀면 아주 좋네.

---
+++ 알고리즘 분류/브루트_포스/1018_체스판_다시_칠하기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+sz_chess = 8
+
+board = [input().rstrip() for _ in range(n)]
+
+min_cnt = 2500
+for i in range(0, (n-sz_chess)+1):
+    for j in range(0, (m-sz_chess)+1):
+        cnt_b = 0
+        cnt_w = 0
+        for ii in range(i, i+sz_chess):
+            for jj in range(j, j+sz_chess):
+                if (ii+jj)%2==0 and board[ii][jj] == 'W':
+                    cnt_b += 1
+                elif (ii+jj)%2!=0 and board[ii][jj] == 'B':
+                    cnt_b += 1
+                elif (ii+jj)%2==0 and board[ii][jj] == 'B':
+                    cnt_w += 1
+                elif (ii+jj)%2!=0 and board[ii][jj] == 'W':
+                    cnt_w += 1
+        min_cnt = min(min_cnt,cnt_b,cnt_w)
+print(min_cnt)
